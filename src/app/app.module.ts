84b290bfe395dd474c1b54ad5a03405e4a444c36
Ricky Marasigan <rickysoliman@gmail.com>
2023-06-16 09:36:18 -0400

    move timer folder into game folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { TimerComponent } from './components/timer/timer.component';
+import { TimerComponent } from './components/game/timer/timer.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { GameComponent } from './components/game/game.component';
 import { GameOverComponent } from './components/game-over/game-over.component';
